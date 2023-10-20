57f11b46138e1a13885f88d62043055f5117bb2d
Jilshak <mohd.jilshak@gmail.com>
2023-10-20 10:25:30 +0530

    basic curd operation completed

--- Notes/lesson3.py
+++ Notes/lesson3.py
@@ -1,2 +1,10 @@
 # QUERY PARAMETERS
 
+# app.get()
+# async def get_item(item_id):
+#   return {"Message":"the argument given there in the function is a query parameter without anything in the route"}
+# 
+# 
+# 
+# 
+# 
--- main.py
+++ main.py
@@ -1,20 +1,57 @@
 from fastapi import FastAPI
+from models import Todo
 
 app = FastAPI()
 
 
 @app.get('/')
 async def root():
-    return {'message': 'Hello world'}
+    return {"Message": "This is the root page"}
 
-@app.post('/', description="This is the first route")
-async def the_post_route():
-    return {'message': 'hello from post route'}
+todos = []
 
-@app.get('/items')
-async def get_list_items():
-    return {"Message":"this is the list items"}
+# get all todo
 
-@app.get('/items/{item_id}')
-async def get_item(item_id : int):
-    return {"item_id": item_id}
+
+@app.get("/todos")
+async def get_todos():
+    return {"Todos": todos}
+
+# get a single todo
+
+@app.get('/todos/{todo_id}')
+async def get_single_todo(todo_id : int):
+    for todo in todos:
+        if todo.id == todo_id:
+            return {"Todo": todo}
+    return {"Message": "item is not present"}
+
+# create a todo
+
+
+@app.post('/todos')
+async def cretate_todos(todo : Todo):
+    print("The new todo is: ", todo)
+    todos.append(todo)
+    return {'Created Todos': todos}
+
+# update a todo
+@app.put('/todos/{todo_id}')
+async def update_todo(todo_id:int, todo: Todo):
+    for i in todos:
+        if i.id == todo_id:
+            i.id = todo_id
+            i.item = todo.item
+            return {'Todo':i}
+    return {'Message': "No todos found by the id you have given"}
+            
+
+# delete a todo
+@app.delete('/todos/{todo_id}')
+async def delete_todo(todo_id: int):
+    for i in todos:
+        if i.id == todo_id:
+            todos.remove(i)
+        else:
+            pass
+    return {"Message":todos}
